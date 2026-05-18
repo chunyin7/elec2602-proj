0ad567de3fae4e12c23ae9a904f2b9cf564cc219
milesng <miles.c.y.ng@gmail.com>
2026-05-18 19:25:50 +1000

    extension

--- reg_decoder.v
+++ reg_decoder.v
@@ -1,15 +1,16 @@
 module reg_decoder(reg_code, enable);
   input [2:0] reg_code;
-  output reg [4:0] enable;
+  output reg [8:0] enable;
 
-  always @(*) begin 
+  always @(*) begin
     case (reg_code)
-      3'b000:  enable = 5'b00001; // r0
-      3'b001:  enable = 5'b00010; // r1
-      3'b010:  enable = 5'b00100; // r2
-      3'b011:  enable = 5'b01000; // g
-      3'b100:  enable = 5'b10000; // a
-      default: enable = 5'b00000; 
+      3'b000:  enable = 9'b000000001; // r0
+      3'b001:  enable = 9'b000000010; // r1
+      3'b010:  enable = 9'b000000100; // r2
+      3'b011:  enable = 9'b000001000; // g
+      3'b100:  enable = 9'b000010000; // a
+      3'b110:  enable = 9'b000100000; // status
+      default: enable = 0;
     endcase
   end
 endmodule
